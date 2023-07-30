de206b031fda194b3683f5b77363400f10e3708e
Romeo <kilazlove@gmail.com>
2023-07-30 19:30:28 +0800

    change: prompt tpye of user btns

--- app/screens/PromptTypeOfUserScreen.js
+++ app/screens/PromptTypeOfUserScreen.js
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text } from "react-native";
 import { TouchableOpacity } from "react-native";
 
 import { StackActions } from "@react-navigation/native";
@@ -9,8 +9,45 @@ export default function PromptTypeOfUserScreen() {
   const navigation = useNavigation();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>PromptTypeOfUserScreen</Text>
+      <Text style={{ fontSize: 20, padding: 20 }}>
+        What type of user are you?
+      </Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.dispatch(StackActions.replace("ImageUploadScreen"));
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            padding: 8,
+            borderWidth: 1,
+            borderRadius: 50,
+            margin: 10,
+          }}
+        >
+          Service Provider
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.dispatch(StackActions.replace("ImageUploadScreen"));
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            padding: 8,
+            borderWidth: 1,
+            borderRadius: 50,
+            margin: 10,
+          }}
+        >
+          Customer
+        </Text>
+      </TouchableOpacity>
       <Text
+        style={{ fontSize: 18, padding: 5, opacity: 0.4, fontWeight: "bold" }}
         onPress={() => {
           navigation.dispatch(StackActions.replace("ImageUploadScreen"));
         }}
